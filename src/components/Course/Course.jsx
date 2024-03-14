f34118cacac7e2a4b1fffcecdd8905cd053ccab3
mdsaifulislamrafel <mdsaifulisalmrafel@gmail.com>
2024-03-14 16:05:37 +0600

    some costomize design

--- src/components/Course/Course.jsx
+++ src/components/Course/Course.jsx
@@ -5,20 +5,22 @@ const Course = ({ singleCourse }) => {
     const credit = singleCourse.reduce((p, c) => p + c.credit, 0)
     return (
         <div className='w-3/12 '>
-            <div className='bg-white border-2 my-5 rounded-lg p-8 space-y-4'>
-                <h1>Courses :{singleCourse.length} </h1>
-                <h4>Course Hour Remaining {totalCredits - credit} hr</h4>
-                <hr />
-                <h3>Course Name </h3>
+            <div className='bg-white border-2 my-5 rounded-lg p-6 space-y-4'>
+                <div className='bg-cyan-600 p-2 rounded-lg text-white'>
+                    <h1 className='text-2xl font-bold'>Courses :{singleCourse.length} </h1>
+                    <h4 className='text-1xl font-bold'>Course Hour Remaining {totalCredits - credit} hr</h4>
+                </div>
+                <hr className='border-b-2 border-gray-400' />
+                <h3 className='text-2xl font-bold'>Course Name </h3>
                 <ul className='list-decimal'>
                     {
-                        singleCourse.map((single, idex) => <li key={idex}>{single.name}</li>)
+                        singleCourse.map((single, idex) => <li className='text-sm font-medium' key={idex}>{single.name}</li>)
                     }
                 </ul>
-                <hr />
-                <p>Total Credit Hour : {credit}</p>
-                <hr />
-                <p>Total Price : {singleCourse.reduce((p, c) => p + c.price, 0)} USD</p>
+                <hr className='border-b-2 border-gray-400' />
+                <p className='text-xl font-semibold'>Total Credit Hour : {credit}</p>
+                <hr className='border-b-2 border-gray-400' />
+                <p className='text-xl font-extrabold'>Total Price : {singleCourse.reduce((p, c) => p + c.price, 0)} USD</p>
             </div>
         </div>
     );
